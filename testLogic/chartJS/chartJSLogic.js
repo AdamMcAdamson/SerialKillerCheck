447fd1d997c7a44151987e136ea2245ade8e1aa4
David Cunningham <David Cunningham>
2017-04-04 10:51:32 -0500

    made a pretty chart

---
+++ testLogic/chartJS/chartJSLogic.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function () {
+	console.log("ready");
+
+	// instantiate Chart class
+	var ctx = $("#compareChart");
+	// instantiate Radar chart style
+	var killerCompare = new Chart(ctx, {
+    type: "radar",
+    data: {
+    	labels: ["Jan", "Feb", "March", "April", "May"],
+    	datasets: [{
+    		label: "Temperature (in Farenheit)",
+    		data: [10, 20, 30, 40, 50],
+    		backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+    	}]
+    
+    },
+
+
+
+    options: {
+            scale: {
+                reverse: true,
+                ticks: {
+                    beginAtZero: true
+                }
+            }
+    }
+});
+
+
+});
