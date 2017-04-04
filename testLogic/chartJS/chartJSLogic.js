1c4451a576226964ca5b6ce5d946cba21d5eb6e5
David Cunningham <David Cunningham>
2017-04-04 11:17:06 -0500

    added second dataset to chart

--- testLogic/chartJS/chartJSLogic.js
+++ testLogic/chartJS/chartJSLogic.js
@@ -1,49 +1,69 @@
+$(document).ready(function() {
+    console.log("ready");
 
-$(document).ready(function () {
-	console.log("ready");
+    // instantiate Chart class
+    var ctx = $("#compareChart");
+    // instantiate Radar chart style
+    var killerCompare = new Chart(ctx, {
+            type: "radar",
+            data: {
+                labels: ["Jan", "Feb", "March", "April", "May"],
+                datasets: [{
+                        label: "2013 Temperature Avgs (in Farenheit)",
+                        data: [10, 20, 30, 40, 50],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                 	   },
+                 	   {
+                        label: "2015 Temperature Avgs (in Farenheit)",
+                        data: [15, 25, 35, 45, 50],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    	}]
 
-	// instantiate Chart class
-	var ctx = $("#compareChart");
-	// instantiate Radar chart style
-	var killerCompare = new Chart(ctx, {
-    type: "radar",
-    data: {
-    	labels: ["Jan", "Feb", "March", "April", "May"],
-    	datasets: [{
-    		label: "Temperature (in Farenheit)",
-    		data: [10, 20, 30, 40, 50],
-    		backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-    	}]
-    
-    },
+        },
 
 
 
-    options: {
+        options: {
             scale: {
                 reverse: true,
                 ticks: {
                     beginAtZero: true
                 }
             }
-    }
-});
+        }
+    });
 
 
 });
