7af01ee0a86ef73fc8fbf36e4fdbf5e534788e1c
Dragon <Dragon>
2017-04-12 18:19:44 -0500

    Sharon's js

--- assets/javascript/detect.js
+++ assets/javascript/detect.js
@@ -1,116 +1,102 @@
-//*global variables */
+//Initial test
+console.log("Yes! Hello JavaScript!");
 
-    var API_URL = 'http://api.faceplusplus.com/';
-    var API_KEY = 'GGirhQNvV0_CtFgy4IgSwCASYBqV1Yla';
-    var API_SECRET = 'u-goCfJWEuKHyhCv5GHRDUtImw-f4uve';
+//*global variables */
+// var request = require('request');
+
+var detectURL = "https://api-us.faceplusplus.com/facepp/v3/detect";
+var compareURL = 'https://api-us.faceplusplus.com/facepp/v3/compare';
+var api_key = 'GGirhQNvV0_CtFgy4IgSwCASYBqV1Yla';
+var api_secret = 'u-goCfJWEuKHyhCv5GHRDUtImw-f4uve';
+var faceset = [];
+var confidenceScores = [];
+var face_token1 = "";
+var image_url2 = ["http://murderpedia.org/male.M/images/miyazaki_tsutomu/miyazaki_000.jpg", "http://cdn.escapistmagazine.com/media/global/images/library/deriv/963/963018.jpg"];
+var userPicUrl = "http://cooleysanemia.org/boduw/wp-content/uploads/2012/09/AASmall.jpg";
 
     // error messages
     var messages = {
-        URL_ERROR:   'Invalid URL',
-        LOAD_ERROR:  'Failed to Load',
-        LOADING:     'Loading...',
-        NO_FACE:     'No face detected',
-        NO_CAMERA:   'No camera available'
- 
+        URL_ERROR: 'Invalid URL',
+        LOAD_ERROR: 'Failed to Load',
+        LOADING: 'Loading...',
+        NO_FACE: 'No face detected',
+        NO_CAMERA: 'No camera available'
+    };
+
 
 //take url from submit button
-$("buttonName").click(function(event) {
-var userPicUrl = $('#userPicUrl').val();
-          console.log(userPicUrl);
+// $(".buttonName").click(function(event) {
+//      userPicUrl = $('#userPicUrl').val();
+//      console.log(userPicUrl);
+//         });
+
+$(document).ready(function(){
+ detect();
+//get face token for each sk image and add to faceset
+function detect() {
+    var queryURL = detectURL + '?api_key=' + api_key + '&api_secret=' + api_secret + "&image_url=" + userPicUrl + "&return_landmark=1&return_attributes=gender,age,smiling,headpose,facequality,blur,eyestatus,ethnicity";
+
+$.ajax({
+    url: queryURL,
+    method: "POST",
+    json: true
+}).done(function(err, response, body){    
+    console.log(JSON.stringify(body));
+    console.log(body.faces[0].face_token);
+    face_token1.push(body.faces[0].face_token);
+    console.log("Face token: " + face_token1);    
+    setTimeout(function (){
+  },2000);
+   
+});
+
+function compare() {
+    var queryURL = compareURL + '?api_key=' + api_key + '&api_secret=' + api_secret + "&face_token1=" + face_token1 + "&image_url2=" + image_url2;
+
+$.ajax({
+    url: queryURL,
+    method: "POST",
+    json: true
+}).done(function(err, response, body){    
+    console.log(JSON.stringify(body));
+   console.log("confidence score: " + body.confidence);
+   confidenceScores.push(parseInt(body.confidence));
+   console.log("confidenceScores array: " + confidenceScores);
+   setTimeout(function (){
+  },2000);
+});
 }
 
-//serial killer urls to compare
-var killersUrls =
-url1,
-url2,
-url3;
+ console.log(compare);
 
 
-//send to face++ the user url and killers url
-function (compareURLs) {
-    var queryURL = "https://api-us.faceplusplus.com/facepp/v3/compare" + parameters;
-
-        // AJAX call 
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
 
 
-          // Creating a div to hold the movie
-          var movieDiv = $("<div class='movie'>");
-//look at the results and pull confidense score
-          // Storing the rating data
-          var rating = response.Rated;
-//timeout
-}
-
-
-var parameters = $.param({
-    api_key: API_KEY,
-    api_secret
-    image_url1: userPicUrl,
-    image_url2: 
-    
-
-})
- 
+}}); // ends document.ready, do not remove
+// }
 
 
 
 
 
 
-         * Hide button in input bar if feature not available
-         */
-}
-        function hideInputButton(selector) {
-            var btn = container.find(selector);
-            var url = container.find('.url-field');
-            url.width(btn.outerWidth(true) + url.width());
-            btn.hide();
-        }
 
 
 
 
-        // URL Input
-        container.find('.url-field').
-            focus(function() { $(this).select(); }).
-            mouseup(function() { return false; });
 
-        container.find('.url-form').on('submit', function() {
-            detect($(this).children('.url-field').val());
-            return false;
-        });
+
+//iterate through urls sk
+//times
+//compare the confidense
 
 
 
-            if (options.type === 'url') {
-                xhr.open('GET', API_URL + 'detection/detect?api_key=' + API_KEY + '&api_secret=' + API_SECRET + '&url=' + encodeURIComponent(options.img), true);
-                xhr.send();
-            } else if (options.type === 'dataURI') {
-                xhr.open('POST', API_URL + 'detection/detect?api_key=' + API_KEY + '&api_secret=' + API_SECRET, true);
-                var fd = new FormData();
-                fd.append('img', dataURItoBlob(options.img));
-                xhr.send(fd);
-            } else {
-                options.error();
-            }
-        } else { // fallback to jsonp
-            if (options.type === 'url') {
-                $.ajax({
-                    url: API_URL + 'detection/detect',
-                    data: {
-                        api_key: API_KEY,
-                        api_secret: API_SECRET,
-                        url: options.img
-                    },
-                    dataType: 'jsonp',
-                    success: options.success,
-                    error: options.error,
-                    timeout: 10 * 1000
-                });
-            } else {
-                options.error();
- 
+//get face token for uploaded image
+
+//loop and compare user token to each sk token and record confidense score
+//loop through confidense scores and find highest number
+//output highest number as likelihood of friend to be a sk
+
+
+
