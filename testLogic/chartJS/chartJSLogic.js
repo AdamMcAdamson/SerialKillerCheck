1353d9b6219bb24729d9bb5ddd6e15d1e5b1ff46
David Cunningham <David Cunningham>
2017-04-12 21:19:16 -0500

    chartJS looking good

--- testLogic/chartJS/chartJSLogic.js
+++ testLogic/chartJS/chartJSLogic.js
@@ -1,5 +1,4 @@
-// $(document).ready(function() {
-// });
+
     console.log("ready");
     function mainProgram() {
     // instantiate Chart class
@@ -8,47 +7,29 @@
     var killerCompare = new Chart(ctx, {
             type: "radar",
             data: {
-                labels: ["Gender", "Age", "Ethnicity", "Nose", "Ears"],
+                labels: ["Age", "Eyes", "Nose", "Mouth", "Ears"],
                 datasets: [{
                         label: "You",
-                        data: [10, 20, 30, 40, 50],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
+                        // characteristics populated in eg.js
+                        data: [killerAge2, 20, 30, 40, 50],
+                        backgroundColor: "rgba(179,181,198,0.2)",
+                        borderColor: "rgba(179,181,198,1)",
+                        pointBackgroundColor: "rgba(179,181,198,1)",
+                        pointBorderColor: "#fff",
+                        pointHoverBackgroundColor: "#fff",
+                        pointHoverBorderColor: "rgba(179,181,198,1)",
                         borderWidth: 1
                  	   },
                  	   {
                         label: "Serial Killer",
-                        data: [4, killerAge, 34, 45, 75],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
+                        // characteristics populated in eg.js
+                        data: [killerAge, 24, 34, 45, 75],
+                        backgroundColor: "rgba(255,99,132,0.2)",
+                        borderColor: "rgba(255,99,132,1)",
+                        pointBackgroundColor: "rgba(255,99,132,1)",
+                        pointBorderColor: "#fff",
+                        pointHoverBackgroundColor: "#fff",
+                        pointHoverBorderColor: "rgba(255,99,132,1)",
                         borderWidth: 1
                     	}]
 
@@ -65,6 +46,9 @@
             }
         }
     });
+
+    
+
 }
 
 
